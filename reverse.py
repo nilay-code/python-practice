dd07fae26abca95610db1ff6ada7cf92a681ede0
Nilay Debnath <77850976+nilay-code@users.noreply.github.com>
2021-12-21 19:47:25 +0600

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,18 @@
+
+while True:
+
+    word = input("Before reverse: \n")
+
+    if word == "done":
+        break
+
+    if word[0] == "#":
+        continue
+
+    reversed = ""
+    for letter in word:
+        reversed=letter+ reversed
+
+    print("After reverse:", reversed)
+
+print("Done")
